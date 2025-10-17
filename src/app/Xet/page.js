87d99d9e32094cb98bbbfa91f572ac56b2090518
Gris Mariano <cristiandalive@gmail.com>
2025-10-17 08:08:20 -0300

    V1.2

--- src/app/Xet/page.js
+++ src/app/Xet/page.js
@@ -5,7 +5,7 @@ import TempChatWidget from '../components/TempChatWidget';
 export default function Xet() {
   return (
     <div style={{ padding: '1rem' }}>
-<h2 className="relative z-10 lg:text-5xl text-8xl pb-6  md:text-9xl font-extrabold text-white text-center tracking-widest drop-shadow-lg text-shadow-pink-500 text-shadow-lg">CHAT </h2>
+<h2 className="relative z-10 lg:text-9xl text-8xl pb-6  md:text-9xl font-extrabold text-white text-center tracking-widest drop-shadow-lg text-shadow-pink-500 text-shadow-lg">CHAT </h2>
     
       <TempChatWidget />
     </div>
